5b79a5e2b5f4c84df5b7675d30c9ebc421b9bdcf
manbalboy <manbalboy@hanmail.net>
2021-09-06 23:36:53 +0900

    axios - #3

--- nuxt-shoppingmall/pages/main.vue
+++ nuxt-shoppingmall/pages/main.vue
@@ -1,12 +1,30 @@
 <template>
   <div>
     <p>메인 페이지입니다.</p>
+    {{ products }}
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-  name: 'mainhome'
+  name: 'Mainhome',
+  async asyncData(context) {
+    const response = await axios.get('http://localhost:3000/products')
+    const products = response
+    return { products }
+  },
+
+  data() {
+    return {
+      products: ''
+    }
+  },
+
+  created() {
+
+  }
 }
 </script>
 
