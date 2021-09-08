7750710d87c4e9550172713ab5254dd9699a9967
manbalboy <manbalboy@hanmail.net>
2021-09-08 23:48:26 +0900

    상품목록 구현 -#5

---
+++ nuxt-shoppingmall/api/index.js
@@ -0,0 +1,40 @@
+
+import axios from 'axios'
+
+const instance = axios.create({
+  baseURL: process.env.baseURL,
+})
+
+function fetchProducts() {
+  console.log('11111111111111111111111111111111111');
+  return instance.get(`/products`)
+}
+
+function fetchProductById(id) {
+  return instance.get(`/products/${id}`)
+}
+
+function fetchProductsByKeyword(keyword) {
+  return instance.get(`/products`, {
+    params: {
+      name_like: keyword,
+    },
+  })
+}
+
+// carts
+function fetchCartItems() {
+  return instance.get('/carts')
+}
+
+function createCartItem(cartItem) {
+  return instance.post('/carts', cartItem)
+}
+
+export {
+  fetchProducts,
+  fetchProductById,
+  fetchProductsByKeyword,
+  fetchCartItems,
+  createCartItem,
+}
