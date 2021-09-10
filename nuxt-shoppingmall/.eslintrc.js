d1eb6df629a78e17bb0702ed79b826fe978cc057
manbalboy <manbalboy@hanmail.net>
2021-09-11 00:35:32 +0900

    린트설정 변경

--- nuxt-shoppingmall/.eslintrc.js
+++ nuxt-shoppingmall/.eslintrc.js
@@ -1,20 +1,16 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true
-  },
-  parserOptions: {
-    parser: '@babel/eslint-parser',
-    requireConfigFile: false
-  },
-  extends: [
-    '@nuxtjs',
-    'plugin:nuxt/recommended',
-    'prettier'
-  ],
-  plugins: [
-  ],
-  // add your custom rules here
-  rules: {}
+    root: true,
+    env: {
+        browser: true,
+        node: true,
+    },
+    parserOptions: {
+        parser: '@babel/eslint-parser',
+        requireConfigFile: false,
+    },
+    extends: ['eslint:recommended', 'plugin:vue/recommended', 'prettier'],
+    plugins: ['vue'],
+
+    // add your custom rules here
+    rules: {},
 }
